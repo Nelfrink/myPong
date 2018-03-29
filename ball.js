25c99177b1bd32527b4b41a8f93985d4fbc49739
Nathan <nathan.elfrink@gmail.com>
2018-03-29 16:57:14 -0500

    clean up

--- ball.js
+++ ball.js
@@ -8,22 +8,6 @@ class Ball{
         this.xSpeed = 4;
         
     }
-    
-//    move(){
-//        this.x -= this.xSpeed;
-//        if(this.x <= -10){
-//            this.x = canvasX/2-4;
-//            this.y = canvasY/2-4;
-//            this.xSpeed = 4;
-//        }else if(this.x <= leftPlayer.x + paddleWidth && this.y >= leftStartingY && this.y <= leftStartingY + paddleHeight){
-//            this.xSpeed = -4;
-//        }else if(this.x + 8 >= rightPlayer.x && this.y >= rightStartingY && this.y <= rightStartingX + paddleHeight){
-//            this.xSpeed = 4;
-//        }else{
-//            return;
-//        }
-//        
-//    }
     show(){
         rect(this.x,this.y,8,8);
         print(this.x,this.y);
