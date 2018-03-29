2e3f97423c42c3085d0b84c84aeef1004bcf66a1
Nathan <nathan.elfrink@gmail.com>
2018-03-29 18:04:14 -0500

    balh

--- ball.js
+++ ball.js
@@ -1,18 +1,17 @@
 class Ball{
-    
+
     constructor(X,Y){
-        
+
         this.x = X/2-4;
         this.y = Y/2-4;
         this.ySpeed = 0;
         this.xSpeed = 4;
-        
+
     }
     show(){
         rect(this.x,this.y,8,8);
         print(this.x,this.y);
-        //this.move();
     }
-    
-    
+
+
 }
